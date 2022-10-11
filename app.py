a61f0b4d6afd90d2650b6450025f2978f3029e80
Vithyashagar <68708047+Vithyashagar@users.noreply.github.com>
2022-10-12 01:36:39 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from unittest import result
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+from  ocr_license_plate import get_number
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    result = ""
+    return render_template('index.html')
+
+@app.route('/getnumber', methods =['Post'])
+def getnumber():
+    """
+        For rendering results in index.html GUI
+    """
+    path = request.form['path']
+    result = get_number(path)
+
+    return render_template('index.html', prediction_text = "Numplate is : {}".format(result))
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
